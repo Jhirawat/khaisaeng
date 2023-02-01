38c92d085084c16e27d96f0ad74aa748eee5967a
Jhirawat <andrew291043@gmail.com>
2023-02-01 15:26:09 +0700

    commit

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -5,21 +5,21 @@
 @section('style')
     <style>
         /* body {
-            color: #000;
-            overflow-x: hidden;
-            height: 100%;
-            background-color: #8C9EFF;
-            background-repeat: no-repeat;
-        } */
+                color: #000;
+                overflow-x: hidden;
+                height: 100%;
+                background-color: #8C9EFF;
+                background-repeat: no-repeat;
+            } */
 
         /* .card {
-            z-index: 0;
-            background-color: #ECEFF1;
-            padding-bottom: 20px;
-            margin-top: 90px;
-            margin-bottom: 90px;
-            border-radius: 10px;
-        } */
+                z-index: 0;
+                background-color: #ECEFF1;
+                padding-bottom: 20px;
+                margin-top: 90px;
+                margin-bottom: 90px;
+                border-radius: 10px;
+            } */
 
         .top {
             padding-top: 40px;
@@ -106,9 +106,9 @@
 
         /*Color number of the step and the connector before it*/
         /* #progressbar li.active:before,
-        #progressbar li.active:after {
-            background: #1fff66;
-        } */
+            #progressbar li.active:after {
+                background: #1fff66;
+            } */
 
         #progressbar li.active:before {
             font-family: FontAwesome;
@@ -345,15 +345,13 @@
         .progress {
             height: 20px;
         }
-
-
     </style>
 
 
 
 
 @section('js')
-<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script>
         $(document).ready(function() {
 
@@ -457,23 +455,10 @@
                     <p>USPS <span class="font-weight-bold">234094567242423422898</span></p>
                 </div>
             </div>
-            <!-- Add class 'active' to progress -->
-            {{-- <div class="row d-flex justify-content-center">
-                <div class="col-12">
-                    <ul id="progressbar" class="text-center">
-                        <li class="active step0"></li>
-                        <li class="active step0"></li>
-                        <li class="active step0"></li>
-                        <li class="step0"></li>
-                    </ul>
-                </div>
-            </div> --}}
+
             <div class="row justify-content-between top">
 
-                {{-- <div class="container-fluid">
-                    <div class="row justify-content-center">
-                        <div class="col-11 col-sm-9 col-md-7 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2">
-                            <div class="card px-0 pt-4 pb-0 mt-3 mb-3"> --}}
+
                 <h2 id="heading">Sign Up Your User Account</h2>
                 <p>Fill all form field to go to next step</p>
 
@@ -486,10 +471,7 @@
                         <li id="confirm"><strong>Finish</strong></li>
                     </ul>
 
-                    {{-- <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                    </div> --}}
+
 
                     <br>
                     <!-- fieldsets -->
@@ -503,14 +485,7 @@
                                     <h2 class="steps">Step 1 - 4</h2>
                                 </div>
                             </div>
-                            {{-- <label class="fieldlabels">Email: *</label>
-                            <input type="email" name="email" placeholder="Email Id" />
-                            <label class="fieldlabels">Username: *</label>
-                            <input type="text" name="uname" placeholder="UserName" />
-                            <label class="fieldlabels">Password: *</label>
-                            <input type="password" name="pwd" placeholder="Password" />
-                            <label class="fieldlabels">Confirm Password: *</label>
-                            <input type="password" name="cpwd" placeholder="Confirm Password" /> --}}
+
                         </div>
                         <input type="button" name="next" class="next action-button" value="Next" />
                     </fieldset>
@@ -524,14 +499,7 @@
                                     <h2 class="steps">Step 2 - 4</h2>
                                 </div>
                             </div>
-                            {{-- <label class="fieldlabels">First Name: *</label>
-                            <input type="text" name="fname" placeholder="First Name" />
-                            <label class="fieldlabels">Last Name: *</label>
-                            <input type="text" name="lname" placeholder="Last Name" />
-                            <label class="fieldlabels">Contact No.: *</label>
-                            <input type="text" name="phno" placeholder="Contact No." />
-                            <label class="fieldlabels">Alternate Contact No.: *</label>
-                            <input type="text" name="phno_2" placeholder="Alternate Contact No." /> --}}
+
                         </div>
                         <input type="button" name="next" class="next action-button" value="Next" />
                         <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
@@ -546,10 +514,7 @@
                                     <h2 class="steps">Step 3 - 4</h2>
                                 </div>
                             </div>
-                            {{-- <label class="fieldlabels">Upload Your Photo:</label>
-                            <input type="file" name="pic" accept="image/*">
-                            <label class="fieldlabels">Upload Signature Photo:</label>
-                            <input type="file" name="pic" accept="image/*"> --}}
+
                         </div>
                         <input type="button" name="next" class="next action-button" value="Submit" />
                         <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
@@ -567,18 +532,7 @@
                             <br><br>
                             <h2 class="purple-text text-center"><strong>ชอบคุณที่ใช้การของเรา</strong></h2>
                             <br>
-                            {{-- <div class="row justify-content-center">
-                                <div class="col-3">
-                                    <img src="https://i.imgur.com/GwStPmg.png" class="fit-image">
-                                </div>
-                            </div>
-                            <br><br>
-                            <div class="row justify-content-center">
-                                <div class="col-7 text-center">
-                                    <h5 class="purple-text text-center">You Have Successfully Signed Up
-                                    </h5>
-                                </div>
-                            </div> --}}
+
                         </div>
                     </fieldset>
                 </form>
